d94b44ad160a3f2eb79870910550c246ccb0a51c
Тимофеев Александр Дмитриевич <aleksandr.d.timofeev@domru.ru>
2023-03-11 15:26:47 +0500

    feat(main): Сreated logic for parsing sources into records

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 import fs from "fs";
+import {performance} from "perf_hooks";
 import commandLineArgs from "command-line-args";
 
 import { T_CLI_ARGUMENTS, dirname } from "./arguments";
 import { ResultStream, prepareOutputDir, RECORD_CHUNK_TYPE, LOGS_CHUNK_TYPE } from "./inOut";
 import UserViewport from "./userViewport";
+import TasksQueue from "./tasksQueue";
 import { getSourceUrls, splitIntoRecords, parseRecordData } from "./parse";
 
 
@@ -11,146 +13,100 @@ import { getSourceUrls, splitIntoRecords, parseRecordData } from "./parse";
 const OPTIONS = commandLineArgs(T_CLI_ARGUMENTS);
 const resultStream = new ResultStream();
 const userViewport = new UserViewport();
+const tasksQueue = new TasksQueue({queueConcurrency: 4});
+const parseUtils = {
+  async simpleQuery({url}, selector, {nodeHtml: htmlContext = ""}, {queryAll}) {
+    const {id, current} = await userViewport.querySelector(
+      {url, html: htmlContext},
+      selector,
+      {all: !!queryAll}
+    );
+    await userViewport.closeTab({id, url});
+    return [current].flat(1);
+  },
+  async queryWithUserAction({url}, eventName, targetSelector, {nodeHtml: htmlContext = ""}, {dispatchForAll}, observedSelector, {queryAll, queryAfterEachAction}) {
+    const {id, current} = await userViewport.querySelector(
+      {url, html: htmlContext},
+      observedSelector,
+      {all: !!queryAll, queryAfterEachAction: !!queryAfterEachAction, preUserActions: [[eventName, targetSelector, {dispatchForAll}]]}
+    );
+    await userViewport.closeTab({id, url});
+    return [current].flat(1);
+  }
+};
+const logs = {
+  recordsCount: 0,
+  sourcesCount: 0,
+  timeSpent: 0,
+  timeStart: performance.now(),
+  err: undefined,
+};
+
 
 (async function main() {
   if ( !(OPTIONS.config && (OPTIONS.useStdOutput || OPTIONS.outputDir)) ) {throw new Error("Required process arguments ['config', 'outputDir/useStdOutput'] not defined")}
 
-  if (OPTIONS.useStdOutput) {
-    resultStream
-      .pipe(ResultStream.createFormatter([], false))
-      .pipe(process.stdout)
-      .on("finish", ()=>process.exit());
-  } else {
-    const {recordsFilename, logsFilename} = await prepareOutputDir.call({dirname}, OPTIONS.outputDir);
-    resultStream
-      .pipe(ResultStream.createFilter({excludeTypes: [LOGS_CHUNK_TYPE]}))
-      .pipe(ResultStream.createFormatter())
-      .pipe(fs.createWriteStream(recordsFilename))
-      .on("finish", ()=>process.exit());
-    resultStream
-      .pipe(ResultStream.createFilter({excludeTypes: [RECORD_CHUNK_TYPE]}))
-      .pipe(ResultStream.createFormatter())
-      .pipe(fs.createWriteStream(logsFilename))
-      .on("finish", ()=>process.exit());
-  }
-
   await userViewport.open();
 
-  console.log('it`s test section runed with args:', OPTIONS);
-  const USE_PREPARED_SOURCES = true;
-  const config = {
-    origin: "https://www.ozon.ru",
-    sources: (USE_PREPARED_SOURCES
-      ? [
-        "https://www.ozon.ru/highlight/divany-i-kresla-393575/?page=2&tf_state=Nhc5tjzNg1-pFFGGdbsZ6lCDhEgdMkct7xgbZf5zznnF6OMG",
-        // "https://www.ozon.ru/highlight/divany-i-kresla-393575/?page=3&tf_state=Nhc5tjzNg1-pFFGGdbsZ6lCDhEgdMkct7xgbZf5zznnF6OMG",
-      ]
-      : {
-        "start": "https://www.ozon.ru/category/kresla-15019/",
-        "next": "*[data-widget='megaPaginator'] > *:nth-last-child(1) a ~ a[href]:not(:nth-last-child(1)):not(:nth-child(n+7))"
-      }
-    ),
-
-    records: {
-      seporator: "*[data-widget='searchResultsV2'] > * > *:not(:nth-child(n+3)) a:has(img[src*='jpg'])",
-      separateIntoSources: true,
-      limit: 2,
-      useHtmlInterface: true,
-
-      store: {
-        "characteristics": [
-          "#section-characteristics > *:nth-child(2)",
-          {
-            forEach: false,
-            current: ($, node)=>Object.fromEntries([].slice.call(node.children()).map((child)=>[
-              $(child.children[0]).text(),
-              [].slice.call($('dl', child))
-                .reduce((res, {children: [dt, dd]})=>{
-                  res[$(dt).text().replaceAll(/:( )*$/g, '').trim()] = $(dd).text();
-                  return res
-                }, {})
-            ]))
-          }
-        ]
-      },
-      scheme: {
-        "source": "@source",
-
-        "name": "*[data-widget='webProductHeading']",
-        "desc": [
-          "#section-description",
-          (...[, , res])=>(res
-            .replaceAll(/\\n/g, '')
-            .trim()
-            .replace(/[!]?Показать полностью[ \\s]*$/m, '')
-            .replace(/^[ \\s]*Описание[ \\s]*/m, '')
-            .trim()
-            .replace(/^[ \\s]*О товаре[ \\s]*/m, '')
-          ),
-        ],
-        "rate": [
-          "*[data-widget='webReviewProductScore'] *[style*='width']",
-          (...[, node])=>(node.css('width').slice(0, -1) / 20),
-        ],
-        "cover": [
-          {
-            queryAll: true,
-            queryAfterEachAction: true,
-            current: [
-              "@emit('click', '*[data-widget=\'webGallery\'] *[data-index]:not(:nth-child(n+5))', {dispatchForAll: true}, '*[data-widget=\'webGallery\'] img[fetchpriority=\'high\']')",
-            ]
-          },
-          {
-            forEach: true,
-            current: (...[, node])=>(node.attr("src"))
-          }
-        ],
-
-        "warrantyTime": "@store('characteristics/Дополнительные/Гарантийный срок')",
-      }
-    }
-  };
-  const utils = {
-    async simpleQuery({url}, selector, {nodeHtml: htmlContext = ""}, {queryAll}) {
-      return [await userViewport.querySelector(
-        {url, html: htmlContext},
-        selector,
-        {all: !!queryAll}
-      )].flat(1);
-    },
-    async queryWithUserAction({url}, eventName, targetSelector, {nodeHtml: htmlContext = ""}, {dispatchForAll}, observedSelector, {queryAll, queryAfterEachAction}) {
-      return [await userViewport.querySelector(
-        {url, html: htmlContext},
-        observedSelector,
-        {all: !!queryAll, queryAfterEachAction: !!queryAfterEachAction, preUserActions: [[eventName, targetSelector, {dispatchForAll}]]}
-      )].flat(1);
-    }
+  if (OPTIONS.useStdOutput) {
+    resultStream.bindWithEnd(
+      resultStream
+        .pipe(ResultStream.createFormatter(...[OPTIONS.config.format ? (OPTIONS.config.format || []) : [[], false]]))
+        .pipe(process.stdout)
+    );
+  } else {
+    const {recordsFilename, logsFilename} = await prepareOutputDir.call({dirname}, OPTIONS.outputDir);
+    resultStream.bindWithEnd(
+      resultStream
+        .pipe(ResultStream.createFilter({excludeTypes: [LOGS_CHUNK_TYPE]}))
+        .pipe(ResultStream.createFormatter(...(OPTIONS.config.format || [])))
+        .pipe(fs.createWriteStream(recordsFilename)),
+      resultStream
+        .pipe(ResultStream.createFilter({excludeTypes: [RECORD_CHUNK_TYPE]}))
+        .pipe(ResultStream.createFormatter(...(OPTIONS.config.format || [])))
+        .pipe(fs.createWriteStream(logsFilename))
+    );
   }
 
-  const getSourceUrlsRes = await getSourceUrls.call({config, utils});
-  console.log("getSourceUrls: ", getSourceUrlsRes, "\n");
+  tasksQueue.on("solveTask", (res)=>(res && resultStream.write(res)));
+  tasksQueue.on("finish", ()=>{
+    logs.timeSpent = performance.now() - logs.timeStart;
+    resultStream.end(logs);
+    resultStream.on("finishPipeLine", ()=>process.exit(0));
+  })
+  tasksQueue.on("error", (err)=>{
+    logs.error = err;
+    resultStream.end(logs);
+    resultStream.on("finishPipeLine", ()=>process.exit(1));
+  })
 
-  await (async function loop(i) {
-    if ( !(i in getSourceUrlsRes) ) {return}
-    const url = getSourceUrlsRes[i];
+  const allSourcesAlreadyFound = OPTIONS.config.sources instanceof Array;
+  const parseContext = {utils: parseUtils, config: OPTIONS.config};
+  let sources = (allSourcesAlreadyFound ? OPTIONS.config.sources : [OPTIONS.config.sources.start]).filter(url=>url).map(url=>({url}));
+  let remainedRecordsCount = Number(OPTIONS.config.records.limit || Infinity);
+  let isEmptyResult = false;
 
-    const {current: items, isUrls} = await splitIntoRecords.call({config, utils}, {url});
-    console.log("splitIntoRecords: ", url, "->", {current: items, isUrls}, "\n");
+  (async function loop(i) {
+    const {url} = sources[i];
+    const {current: items, isUrls} = await splitIntoRecords.call(parseContext, {url});
 
-    await (async function subLoop(j) {
-      if ( !(j in items) ) {return}
-      const subUrl = isUrls ? items[j] : url;
+    if ( !allSourcesAlreadyFound && sources.length < (OPTIONS.config.sources.limit - 1) ) {
+      await getSourceUrls.call(parseContext).then(urls=>urls.map(url=>sources.push({url})));
+      if (sources.length > OPTIONS.config.sources.limit) {sources = sources.slice(0, OPTIONS.config.sources.limit)}
+    };
 
-      const parseRecordDataRes = await parseRecordData.call({config, utils}, {url: subUrl, html: isUrls ? undefined : items[j]});
-      console.log("parseRecordData: ", url, "->", parseRecordDataRes, "\n");
+    if ((remainedRecordsCount -= items.length) >= 0) {
+      logs.recordsCount += items.length;
+      items.forEach((item, i)=>tasksQueue.push(async ()=>{
+        const [html, subUrl] = isUrls ? [undefined, item] : [item, url];
+        const record = await parseRecordData.call(parseContext, {html, url: subUrl});
+        return record;
+      }));
 
-      await subLoop(j + 1);
-    })(0);
-
-    await loop(i + 1);
-  })(0)
-
-  process.exit();
+      logs.sourcesCount += 1;
+      (i < (sources.length - 1)) && loop(i + 1);
+    }
+  })(0);
 })()
 
 process.on("exit", async (code = 0)=>{
